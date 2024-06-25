b8dbad6b2024261b5e42156de18ec1da25b795f0
frckbrice <maebrie2017@gmail.com>
2024-06-25 16:28:41 +0100

    update inter from layout

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={fontSans.className}>
         <UserAuthGuardProvider> {children} </UserAuthGuardProvider>
       </body>
     </html>
