91ecb89abe7bd200f47f145f60316244555ffd77
GaelTikeng <Gaelinho@yahoo.fr>
2024-06-25 15:11:41 +0100

    shadcn installed and configured successfully

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import React from 'react'
+
+type Props = {}
+
+export default function Home({}: Props) {
   return (
     <main>
       <h1>Hello From R - Devs</h1>
