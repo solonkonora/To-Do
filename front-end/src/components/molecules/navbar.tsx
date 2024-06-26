127f72a3c2039e560d022f81759dce53f44b02d7
Rash <orashusedmund@gmail.com>
2024-06-26 18:00:13 +0100

    added nav login / todos functionality

--- front-end/src/components/molecules/navbar.tsx
+++ front-end/src/components/molecules/navbar.tsx
@@ -4,11 +4,10 @@ import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { TextLogo } from "../ui/text-logo";
 import { useAppContext } from "@/providers/context/app-context";
+import Link from "next/link";
 
 export default function Navbar() {
   const { currentUser } = useAppContext();
-
-  console.log({ currentUser })
 
   return (
     <nav className="w-full h-[min(20vh,_90px)] px-6 py-5 bg-secondary-color flex items-center justify-between">
@@ -17,9 +16,9 @@ export default function Navbar() {
       <div className="w-fit flex items-center justify-center gap-2 text-primary-color hover:cursor-pointer">
         <FaUserCircle size={40} />
 
-        <span className="font-semibold">
+        <Link href={currentUser?.username ? "/todos" : "/login"} className="font-semibold">
           {currentUser?.username || "Login"}
-        </span>
+        </Link>
       </div>
     </nav>
   );
