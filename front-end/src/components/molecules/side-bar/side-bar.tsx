1df74929b1c1b054cbd6c72b35a6d16dbde24301
ewi josepha <ewijosepha@gmail.com>
2024-06-27 14:59:36 +0200

    side bar fix

--- front-end/src/components/molecules/side-bar/side-bar.tsx
+++ front-end/src/components/molecules/side-bar/side-bar.tsx
@@ -1,4 +1,4 @@
-import { CircleUser, LayoutGrid, LogOut, Plus } from "lucide-react";
+import {  LayoutGrid, LogOut, Plus } from "lucide-react";
 import Link from "next/link";
 
 const dashboardElements = [
@@ -20,14 +20,14 @@ export default function Sidebar() {
     <div
       className="w-[40vw] max-w-[300px] flex flex-col items-center justify-start gap-6 pt-8 bg-primary-color rounded-none"
     >
-      {dashboardElements.map((textIcon, index) => (
-        <Link className="w-[90%] flex items-center justify-start gap-4 cursor-pointer" key={index} href={textIcon.href}>
+      {dashboardElements.map((textIcon) => (
+        <Link className="w-[90%] flex items-center justify-start gap-4 cursor-pointer" key={textIcon.href} href={textIcon.href}>
           <div className="flex flex-col items-center gap-4">
             <div className="w-[40px] h-[35px] rounded-sm">
               <span>{textIcon.icon}</span>
             </div>
           </div>
-          <div className="w-[90%] h-[35px] rounded-sm text-tertiary-color] hover:text-blue-950">
+          <div className="w-[90%] h-[35px] rounded-sm text-tertiary-color hover:text-blue-950">
             <span>{textIcon.text}</span>
           </div>
         </Link>
@@ -35,7 +35,7 @@ export default function Sidebar() {
       <Link href="/logout" className="w-[90%] flex items-center justify-start gap-4 mt-80 cursor-pointer" >
         <div className="flex flex-col items-center gap-4">
           <div className="w-[40px] h-[35px] rounded-sm">
-            <LogOut className="text-tertiary-color]" />
+            <LogOut className="text-tertiary-color" />
           </div>
         </div>
         <div className="w-[90%] h-[35px] rounded-sm text-tertiary-color hover:text-blue-950">
