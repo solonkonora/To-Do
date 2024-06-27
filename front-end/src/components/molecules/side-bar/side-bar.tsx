9ec9646dbbb10c162db0d5128a2388a8582cf019
ewi josepha <ewijosepha@gmail.com>
2024-06-27 14:36:07 +0200

    updating code by removing all hard coded color code

--- front-end/src/components/molecules/side-bar/side-bar.tsx
+++ front-end/src/components/molecules/side-bar/side-bar.tsx
@@ -1,53 +1,47 @@
 import { CircleUser, LayoutGrid, LogOut, Plus } from "lucide-react";
 import Link from "next/link";
 
+const dashboardElements = [
+  {
+    text: "Todos",
+    icon: <LayoutGrid className=" text-tertiary-color" />,
+    href: "/todos"
+  },
+
+  {
+    text: "Add Task",
+    icon: <Plus className="text-tertiary-color" />,
+    href: "/todos/add-new"
+  },
+];
+
 export default function Sidebar() {
-  const dashboardElements = [
-    {
-      text: "Todos",
-      icon: <LayoutGrid className=" text-[#ffddd2]" />,
-      href: "/todos"
-    },
-  
-    {
-      text: "Add Task",
-      icon: <Plus className="text-[#ffddd2]" />,
-      href: "/todos/add-new"
-    },
-  ];
-
   return (
-    <>
-      <div
-        className="w-[40vw] max-w-[300px] flex flex-col items-center justify-start gap-6 pt-8 bg-primary-color rounded-none"
-      >
-        {dashboardElements.map((textIcon, index) => (
-
-          <Link className="w-[90%] flex items-center justify-start gap-4 cursor-pointer" key={index} href={textIcon.href}>
-            <div className="flex flex-col items-center gap-4">
-              <div className="w-[40px] h-[35px] rounded-sm">
-                <span>{textIcon.icon}</span>
-              </div>
+    <div
+      className="w-[40vw] max-w-[300px] flex flex-col items-center justify-start gap-6 pt-8 bg-primary-color rounded-none"
+    >
+      {dashboardElements.map((textIcon, index) => (
+        <Link className="w-[90%] flex items-center justify-start gap-4 cursor-pointer" key={index} href={textIcon.href}>
+          <div className="flex flex-col items-center gap-4">
+            <div className="w-[40px] h-[35px] rounded-sm">
+              <span>{textIcon.icon}</span>
             </div>
-            <div className="w-[90%] h-[35px] rounded-sm text-[#ffddd2] hover:text-blue-950">
-              <span>{textIcon.text}</span>
-            </div>
-          </Link>
-
-        ))}
-
-          <Link href="/logout" className="w-[90%] flex items-center justify-start gap-4 mt-80 cursor-pointer" >
-            <div className="flex flex-col items-center gap-4">
-              <div className="w-[40px] h-[35px] rounded-sm">
-                <LogOut className="text-[#ffddd2]" />
-              </div>
-            </div>
-            <div className="w-[90%] h-[35px] rounded-sm text-[#ffddd2] hover:text-blue-950">
-              <span>Logout</span>
-            </div>
-          </Link>
-
-      </div>
-    </>
+          </div>
+          <div className="w-[90%] h-[35px] rounded-sm text-tertiary-color] hover:text-blue-950">
+            <span>{textIcon.text}</span>
+          </div>
+        </Link>
+      ))}
+      <Link href="/logout" className="w-[90%] flex items-center justify-start gap-4 mt-80 cursor-pointer" >
+        <div className="flex flex-col items-center gap-4">
+          <div className="w-[40px] h-[35px] rounded-sm">
+            <LogOut className="text-tertiary-color]" />
+          </div>
+        </div>
+        <div className="w-[90%] h-[35px] rounded-sm text-tertiary-color hover:text-blue-950">
+          <span>Logout</span>
+        </div>
+      </Link>
+    </div>
   );
 }
