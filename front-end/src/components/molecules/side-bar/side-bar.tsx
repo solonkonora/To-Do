ce890f3fb59b1083a9d3020622a09960ec0d7482
ewi josepha <ewijosepha@gmail.com>
2024-06-27 10:31:57 +0200

    updating side bar by adding text

--- front-end/src/components/molecules/side-bar/side-bar.tsx
+++ front-end/src/components/molecules/side-bar/side-bar.tsx
@@ -1,17 +1,41 @@
 export default function Sidebar() {
-  return (
-    <div // acting as sidebar
-      className="w-[40vw] max-w-[300px] hidden md:flex flex-col items-center justify-start gap-6 pt-8 bg-primary-color rounded-none"
-    >
-      {
-        Array.from({ length: 4 }, (_, i) => i).map((index) => (
-          <div key={index} className="w-[90%] flex items-center justify-start gap-4">
-            <div className="w-[40px] h-[35px] rounded-sm bg-[#ffddd2]" />
+  const dashboardElements = [
+    {
+      text: "Dashboard",
+      icon: ""
+    },
+    {
+      text: "All Tasks",
+      icon: ""
+    },
+    {
+      text: "Task Completed",
+      icon: ""
+    },
+    {
+      text: "Add Task",
+      icon: ""
+    }
+  ];
 
-            <div className="w-[90%] h-[35px] rounded-sm bg-[#ffddd2]" />
-          </div>
-        ))
-      }
-    </div>
+  return (
+    <>
+      <div
+        className="w-[40vw] max-w-[300px] flex flex-col items-center justify-start gap-6 pt-8 bg-primary-color rounded-none"
+      >
+        <div className="w-[90%] flex items-center justify-start gap-4">
+          {dashboardElements.map((textIcon, index) => (
+            <div key={index} className="flex flex-col items-center gap-4">
+              <div className="w-[40px] h-[35px] rounded-sm">
+                <span>{textIcon.icon}</span>
+              </div>
+              <div className="w-[90%] h-[35px] rounded-sm bg-[#ffddd2]">
+                <span>{textIcon.text}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
