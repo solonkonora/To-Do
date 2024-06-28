217b467ac0dafc62a1d93f639b46f28def107e8b
ewi josepha <ewijosepha@gmail.com>
2024-06-28 21:30:29 +0200

    adding avatar for the team members and thier github links

--- front-end/src/featuers/home-page/home-page.tsx
+++ front-end/src/featuers/home-page/home-page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import Team from "./team";
 
 export default function HomePage() {
   return (
@@ -9,15 +10,19 @@ export default function HomePage() {
         <p className="text-primary-color font-bold text-3xl">R-Devs Todo App</p>
       </div>
 
-      <Image src="/assets/landing.svg" alt="My Icon" width={500} height={500} className="" />
+      <Image src="/assets/landing.svg" alt="My Icon" width={500} height={500}/>
 
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col w-[80%] items-center pb-10 border-b-2 border-background-primary-color">
         <p className="text-secondary-color pt-10">Create track, and manage your daily tasks</p>
     
         <Link href="/todos" className="bg-primary-color text-white px-16 py-2 rounded-md mt-6">
           Get Started
         </Link>
       </div>
+      <div className=" flex items-center pt-10">
+        <p className="text-primary-color text-4xl">Meet our Team</p>
+      </div>
+      <Team />
     </main>
   );
 };
